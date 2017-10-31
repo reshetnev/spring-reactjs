4896cf6bc86c30bb2c04dcf92edd8c75e2cb3f95
Aleksandr Reshetnev <a.v.reshetnev@gmail.com>
2017-10-31 14:52:23 +0300

    Deleted lombok. Formatted.

--- security/src/main/java/com/epam/reshetnev/spring/reactjs/EventHandler.java
+++ security/src/main/java/com/epam/reshetnev/spring/reactjs/EventHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.epam.reshetnev.spring.reactjs;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,48 +9,42 @@ import org.springframework.hateoas.EntityLinks;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
-// tag::code[]
 @Component
 @RepositoryEventHandler(Employee.class)
 public class EventHandler {
 
-	private final SimpMessagingTemplate websocket;
+    private final SimpMessagingTemplate websocket;
 
-	private final EntityLinks entityLinks;
+    private final EntityLinks entityLinks;
 
-	@Autowired
-	public EventHandler(SimpMessagingTemplate websocket, EntityLinks entityLinks) {
-		this.websocket = websocket;
-		this.entityLinks = entityLinks;
-	}
+    @Autowired
+    public EventHandler(SimpMessagingTemplate websocket, EntityLinks entityLinks) {
+        this.websocket = websocket;
+        this.entityLinks = entityLinks;
+    }
 
-	@HandleAfterCreate
-	public void newEmployee(Employee employee) {
-		this.websocket.convertAndSend(
-				WebSocketConfiguration.MESSAGE_PREFIX + "/newEmployee", getPath(employee));
-	}
+    @HandleAfterCreate
+    public void newEmployee(Employee employee) {
+        this.websocket.convertAndSend(WebSocketConfiguration.MESSAGE_PREFIX + "/newEmployee", getPath(employee));
+    }
 
-	@HandleAfterDelete
-	public void deleteEmployee(Employee employee) {
-		this.websocket.convertAndSend(
-				WebSocketConfiguration.MESSAGE_PREFIX + "/deleteEmployee", getPath(employee));
-	}
+    @HandleAfterDelete
+    public void deleteEmployee(Employee employee) {
+        this.websocket.convertAndSend(WebSocketConfiguration.MESSAGE_PREFIX + "/deleteEmployee", getPath(employee));
+    }
 
-	@HandleAfterSave
-	public void updateEmployee(Employee employee) {
-		this.websocket.convertAndSend(
-				WebSocketConfiguration.MESSAGE_PREFIX + "/updateEmployee", getPath(employee));
-	}
+    @HandleAfterSave
+    public void updateEmployee(Employee employee) {
+        this.websocket.convertAndSend(WebSocketConfiguration.MESSAGE_PREFIX + "/updateEmployee", getPath(employee));
+    }
 
-	/**
-	 * Take an {@link Employee} and get the URI using Spring Data REST's {@link EntityLinks}.
-	 *
-	 * @param employee
-	 */
-	private String getPath(Employee employee) {
-		return this.entityLinks.linkForSingleResource(employee.getClass(),
-				employee.getId()).toUri().getPath();
-	}
-
+    /**
+     * Take an {@link Employee} and get the URI using Spring Data REST's
+     * {@link EntityLinks}.
+     *
+     * @param employee
+     */
+    private String getPath(Employee employee) {
+        return this.entityLinks.linkForSingleResource(employee.getClass(), employee.getId()).toUri().getPath();
+    }
 }
-// end::code[]
--- security/src/main/java/com/epam/reshetnev/spring/reactjs/SecurityConfiguration.java
+++ security/src/main/java/com/epam/reshetnev/spring/reactjs/SecurityConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.epam.reshetnev.spring.reactjs;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,39 +8,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-// tag::code[]
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private SpringDataJpaUserDetailsService userDetailsService;
+    @Autowired
+    private SpringDataJpaUserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.userDetailsService(this.userDetailsService)
-				.passwordEncoder(Manager.PASSWORD_ENCODER);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(Manager.PASSWORD_ENCODER);
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/built/**", "/main.css").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.defaultSuccessUrl("/", true)
-				.permitAll()
-				.and()
-			.httpBasic()
-				.and()
-			.csrf().disable()
-			.logout()
-				.logoutSuccessUrl("/");
-	}
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/built/**", "/main.css").permitAll().anyRequest().authenticated().and()
+                .formLogin().defaultSuccessUrl("/", true).permitAll().and().httpBasic().and().csrf().disable().logout()
+                .logoutSuccessUrl("/");
+    }
 }
-// end::code[]
